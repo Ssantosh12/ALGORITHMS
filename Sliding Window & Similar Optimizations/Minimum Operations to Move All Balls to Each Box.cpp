486646f8c17f5361e810477adfc684b46f10b283
Santosh Kumar <51458614+Ssantosh12@users.noreply.github.com>
2021-06-11 09:18:21 +0530

    Update Minimum Operations to Move All Balls to Each Box.cpp

--- Sliding Window & Similar Optimizations/Minimum Operations to Move All Balls to Each Box.cpp
+++ Sliding Window & Similar Optimizations/Minimum Operations to Move All Balls to Each Box.cpp
@@ -1,6 +1,9 @@
 class Solution {
-public:
-    vector<int> minOperations(string boxes) {
+public:                                         // V.V.V IMPORTANT OPTIMIZATION
+    vector<int> minOperations(string boxes) { //same kind of thinking applied in Hackwithinfy problem
+                                              //THINKING: no need to calculate no of moves for each posn again (that would result in O(n^2) time complexity)
+                                              //just calculate it first time in O(n) for 1st posn and then for each posn the moves are  modified
+                                             //  in O(1) time complexity for other posns
         int i,j,left=0,right=0,sum=0;
         for(i=0;i<boxes.size();i++){
             if(boxes[i]=='1'){
