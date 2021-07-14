834491618103a4214971d974a06a3023917f5814
Santosh Kumar <51458614+Ssantosh12@users.noreply.github.com>
2021-07-14 19:21:43 +0530

    Create Delete duplicate-value nodes from a sorted linked list.cpp

---
+++ Linked Lists/Delete duplicate-value nodes from a sorted linked list.cpp
@@ -0,0 +1,27 @@
+/*
+ * Complete the 'removeDuplicates' function below.
+ *
+ * The function is expected to return an INTEGER_SINGLY_LINKED_LIST.
+ * The function accepts INTEGER_SINGLY_LINKED_LIST llist as parameter.
+ */
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+
+SinglyLinkedListNode* removeDuplicates(SinglyLinkedListNode* llist) {
+    SinglyLinkedListNode *ptr=llist;
+    while(ptr->next!=NULL){
+        if(ptr->data==ptr->next->data)
+           ptr->next=ptr->next->next;
+        else ptr=ptr->next;   
+    }
+    
+    return llist;
+}
