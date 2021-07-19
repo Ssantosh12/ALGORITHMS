548a9bca001c720f7617212a6feefdcd79d9c6ad
Santosh Kumar <51458614+Ssantosh12@users.noreply.github.com>
2021-07-19 18:10:58 +0530

    Update Connected Components ( With Recursion ).cpp

--- Graphs/Connected Components ( With Recursion ).cpp
+++ Graphs/Connected Components ( With Recursion ).cpp
@@ -1,34 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void dfs(int node,vector<bool> &visited, vector<vector<int>> &adj ){
+void dfs(int node, vector<bool> &visited, vector<vector<int>> &adj){
     if(visited[node])
         return;
     cout<<node<<" ";
     visited[node]=true;
-    for(auto x: adj[node]){
-        dfs(x,visited,adj);
-    }
+    for(auto u : adj[node])
+        dfs(u,visited,adj);
 
 }
 
 int main(){
-    int u,v,n,i,j,e,cnt=0;
+
+    int n,e,i,j,u,v,cnt=0;
     scanf("%d%d",&n,&e);
     vector<vector<int>> adj(n);
+    //creating adjacency list
     for(i=0;i<e;i++){
         scanf("%d%d",&u,&v);
         adj[u].push_back(v);
         adj[v].push_back(u);
     }
     vector<bool> visited(n,false);
+    //for counting connected components just increment cnt whenever dfs on 1 component ends
     for(i=0;i<n;i++){
         if(visited[i]==false){
             dfs(i,visited,adj);
-            cout<<"\n";
             cnt++;
+            cout<<"\n";
         }
     }
-
     cout<<cnt<<"\n";
 }
