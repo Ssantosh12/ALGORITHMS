ca6c26a3c22f6a626e1094ac4c7037190d81e218
Santosh Kumar <51458614+Ssantosh12@users.noreply.github.com>
2021-07-24 09:34:10 +0530

    Update BFS (using queue).cpp

--- Graphs/BFS (using queue).cpp
+++ Graphs/BFS (using queue).cpp
@@ -1,19 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void dfs(int node, vector<bool> &visited, vector<vector<int>> &adj){
-    if(visited[node])
-        return;
-    cout<<node<<" ";
-    visited[node]=true;
-    for(auto u : adj[node])
-        dfs(u,visited,adj);
-
-}
-
 int main(){
 
-    int n,e,i,j,u,v,cnt=0;
+    int n,e,i,j,u,v;
     scanf("%d%d",&n,&e);
     vector<vector<int>> adj(n);
     //creating adjacency list
@@ -28,12 +18,12 @@ int main(){
     //we insert the initial node 0 in the queue to start bfs
     q.push(0);
     while(!q.empty()){
-        if(visited[q.front()]==false){  //here we only print the node value if it unvisited
+        if(visited[q.front()]==false){  //here we only print the node value if it is unvisited
              cout<<q.front()<<" ";
              visited[q.front()]=true;
         }
         for(auto u: adj[q.front()]){
-            if(visited[u]==false){
+            if(visited[u]==false){  //neighbourig nodes only added if they are unvisited else it will create infinite loop
                 q.push(u);
             }
         }
