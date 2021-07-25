b40d58487c258ce3a7ab0775d25139acdeaa5728
Santosh Kumar <51458614+Ssantosh12@users.noreply.github.com>
2021-07-25 15:29:20 +0530

    Update Height of a binary tree (using bfs and queue).cpp

--- Graphs/Height of a binary tree (using bfs and queue).cpp
+++ Graphs/Height of a binary tree (using bfs and queue).cpp
@@ -1,1 +1,31 @@
 
+/*The tree node has data, left child and right child 
+class Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+*/
+    int height(Node* root) {
+        // Write your code here.
+        int ht=0;
+        queue<Node *> q;
+        queue<int> qht;
+        q.push(root); 
+        qht.push(0);
+        while(!q.empty()){
+            ht=max(ht,qht.front());
+            if(q.front()->left!=NULL){
+                q.push(q.front()->left);
+                qht.push(qht.front()+1);
+            }
+            if(q.front()->right!=NULL){
+                q.push(q.front()->right);
+                qht.push(qht.front()+1);
+            }
+            q.pop();
+            qht.pop();
+        }
+        return ht;
+    }
