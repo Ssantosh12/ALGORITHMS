76827090e038c843b748b8491b6f4fb55409cf4b
Santosh Kumar <51458614+Ssantosh12@users.noreply.github.com>
2021-08-02 08:30:25 +0530

    Update Goodland Electricity (Hackerrank).cpp

--- Greedy/Goodland Electricity (Hackerrank).cpp
+++ Greedy/Goodland Electricity (Hackerrank).cpp
@@ -14,7 +14,8 @@ int main(){
         if(tind>(n-1))
             tind=n-1;
         cnd=0;
-        for(j=tind;j>=sind-(k-1)&&j>=0;j--){  //NOTE:  
+        for(j=tind;j>=sind-(k-1)&&j>=0;j--){  //NOTE:for each sind the search range is sind-(k-1) to sind+(k-1)
+                                              // we start from sind+(k-1) and move left because we want to build tower as right as possible
             if(a[j]==1){
                 sind=j+k;
                 cnd=1;
