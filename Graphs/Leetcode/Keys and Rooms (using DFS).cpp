f2d88864db3706fdbef6f395626f59b7cc01a43f
Santosh Kumar <51458614+Ssantosh12@users.noreply.github.com>
2021-08-20 09:40:00 +0530

    Update Keys and Rooms (using DFS).cpp

--- Graphs/Leetcode/Keys and Rooms (using DFS).cpp
+++ Graphs/Leetcode/Keys and Rooms (using DFS).cpp
@@ -1,28 +1,21 @@
 class Solution {
 public:
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
-        //we run simple dfs (Here time and space complexity is too much) 
-        int n=rooms.size(),i,j,cnd=1;
+        //using simple dfs
+        int cnd=1,n=rooms.size(),i;
         vector<bool> visited(n,false);
-       // visited[0]=true;
         dfs(0,rooms,visited);
         for(i=0;i<n;i++){
-            if(visited[i]==false){
-                cnd=0;
-                break;
-            }
+            if(visited[i]==false)
+                return false;
         }
-        if(cnd==1)
-            return true;
-        else return false;
+        return true;
     }
-    void dfs(int node,vector<vector<int>> adj, vector<bool> &visited){
+    void dfs(int node,vector<vector<int>> &adj,vector<bool> &visited){ //NOTE: send adjacency list as a reference to the recursive function to reduce time and space complexity
         if(visited[node])
             return;
         visited[node]=true;
-        for(auto x: adj[node]){
-            //if(visited[node]==false)
-                dfs(x,adj,visited);
-        }
+        for(auto x: adj[node])
+            dfs(x,adj,visited);
     }
 };
