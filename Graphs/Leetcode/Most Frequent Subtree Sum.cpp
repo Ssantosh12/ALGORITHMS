19c1d5cd8a43f9f21a38956da7b2d02ec421b329
Santosh Kumar <51458614+Ssantosh12@users.noreply.github.com>
2021-09-15 20:11:37 +0530

    Create Most Frequent Subtree Sum.cpp

---
+++ Graphs/Leetcode/Most Frequent Subtree Sum.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> findFrequentTreeSum(TreeNode* root) {
+        int i,mx=1;
+        map<int,int> mp;//freq of each subtree sum
+        subsum(root,mp);
+        for(auto x :mp)
+            mx=max(mx,x.second);
+        vector<int> vc;
+        for(auto x :mp){
+            if(x.second==mx)
+                vc.push_back(x.first);
+        }
+        return vc;
+    }
+    int subsum(TreeNode *node,map<int,int> &mp){
+        if(node==NULL)
+            return 0;
+        int sum=node->val;  //for subtree sum from this node is um of its val of subtree sum of left node + subtree sum of right node
+        sum=sum+subsum(node->left,mp);  //ask the left child its subtree sum
+        sum=sum+subsum(node->right,mp);   //ask the right its subtree sum
+        mp[sum]++;
+        return sum;
+    }
+};
