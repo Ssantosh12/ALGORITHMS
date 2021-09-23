03cfd37d24f6195938a68386b7b20f14763cb4d1
Santosh Kumar <51458614+Ssantosh12@users.noreply.github.com>
2021-09-23 19:53:57 +0530

    Create Find Minimum in Rotated Sorted Array.cpp

---
+++ Binary Search/Find Minimum in Rotated Sorted Array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findMin(vector<int>& nums) {       //same as finding the 1st index of an element in sorted array(like lowerbound)
+        int left=0,right=nums.size()-1,mid;
+        while(left<right){
+            mid=(left+right)/2;
+            if(nums[mid]<nums[right])
+                right=mid;
+            
+            else
+                left=mid+1;
+            
+        }
+        return nums[left];
+    }
+};
