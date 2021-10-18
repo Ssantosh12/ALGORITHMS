b24e4ddc7a88f64decee0fc80eee3c3f53d6f66e
Santosh Kumar <51458614+Ssantosh12@users.noreply.github.com>
2021-10-18 22:46:09 +0530

    Create Min Subarray (with sum atleast s).cpp

---
+++ Two Pointers/Min Subarray (with sum atleast s).cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    long long int s,n,left=0,right=0,sum=0,mn=100000,i;
+    scanf("%lld%lld",&n,&s);
+    int a[n];
+    bool cnd=false;
+    for(i=0;i<n;i++)
+        scanf("%d",&a[i]);
+    //segment with sum atmost s and with maximum length
+    while(right<n){
+        sum=sum+a[right];
+        if(sum>=s){
+            cnd=true;
+            //mn=min(mn,right-left+1);  //no need to check here, since we need the smallest length, so first we reduce the subarray size from the left side
+                                           //as much as we can and then we check the last valid length of the subarray
+            while(sum>=s){
+                sum=sum-a[left];
+                left++;
+            }
+           
+            mn=min(mn,right-left+2);
+        }
+        right++;
+    }
+    if(cnd)
+       printf("%d\n",mn);
+    else printf("-1\n");
+}
